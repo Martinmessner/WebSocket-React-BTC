9be3ce2d5eb9473a19e291c52ffe4bc80f9ef9ba
Martinmessner <martinmessner95@gmail.com>
2023-06-17 13:17:08 -0300

    Add icons, image and Set loading

--- src/components/WebSocket.jsx
+++ src/components/WebSocket.jsx
@@ -1,77 +1,91 @@
 import { useState, useEffect } from 'react';
+import { SpinnerLoading } from './Spinner';
 
 const WebSocketComponent = () => {
   const [dataBtc, SetdataBtc] = useState(0);
   const [prevDataBtc, SetprevDataBtc] = useState(0);
-  const [showBitcoin, SetshowBitcoin] = useState(true);
   const [color, Setcolor] = useState('');
   const [pricePercentage, SetpricePercentage] = useState('');
   const [percentagePriceValue, SetpercentagePriceValue] = useState('');
+  const [loading, Setloading] = useState(true);
 
   useEffect(() => {
+    Setloading(true);
     if (dataBtc !== 0) {
-      SetshowBitcoin(false);
-
-      document.title = `Precio del Bitcoin: ${dataBtc}`;
+      document.title = `${dataBtc} | BTC.`;
     } else {
-      document.title = 'Active Bitcoin para verlo aqui.';
+      document.title = 'CryptoCode.';
     }
+    Setloading(false);
   }, [dataBtc]);
 
   useEffect(() => {
-    connectWebSocketPercentage();
+    Setloading(true);
+    const connectWebSockets = async () => {
+      await Promise.all([connectWebSocket(), connectWebSocketPercentage()]);
+      Setloading(false);
+    };
+    connectWebSockets();
   }, []);
 
-  const connectWebSocketPercentage = () => {
-    const socketPrice = new WebSocket(
-      'wss://stream.binance.com:9443/ws/btcusdt@ticker'
-    );
+  const connectWebSocketPercentage = async () => {
+    return new Promise((resolve) => {
+      const socketPrice = new WebSocket(
+        'wss://stream.binance.com:9443/ws/btcusdt@ticker'
+      );
 
-    socketPrice.onopen = () => {
-      console.log('Conexión WebSocket Porcentaje establecida');
-    };
+      socketPrice.onopen = () => {
+        console.log('Conexión WebSocket Porcentaje establecida');
+      };
 
-    socketPrice.onmessage = (event) => {
-      const data = JSON.parse(event.data);
-      const percentage = parseFloat(data.P);
-      const percentagePriceValue = parseFloat(data.p);
-      SetpricePercentage(percentage);
-      SetpercentagePriceValue(percentagePriceValue);
-    };
+      socketPrice.onmessage = (event) => {
+        const data = JSON.parse(event.data);
+        const percentage = parseFloat(data.P);
+        const percentagePriceValue = parseFloat(data.p);
+        SetpricePercentage(percentage);
+        SetpercentagePriceValue(percentagePriceValue);
+        resolve(); // Resuelve la promesa una vez que los datos estén listos
+      };
 
-    socketPrice.onclose = () => {
-      console.log('Conexión WebSocket Price cerrada');
-    };
+      socketPrice.onclose = () => {
+        console.log('Conexión WebSocket Price cerrada');
+      };
 
-    return () => {
-      socketPrice.close();
-    };
+      return () => {
+        socketPrice.close();
+      };
+    });
   };
 
-  const connectWebSocket = () => {
-    const socket = new window.WebSocket(
-      'wss://stream.binance.com:9443/ws/btcusdt@trade'
-    );
+  const connectWebSocket = async () => {
+    return new Promise((resolve) => {
+      const socket = new window.WebSocket(
+        'wss://stream.binance.com:9443/ws/btcusdt@trade'
+      );
 
-    socket.onopen = () => {
-      console.log('Conexión WebSocket establecida');
-    };
-    socket.onmessage = async (event) => {
-      const trade = JSON.parse(event.data);
-      const price = parseFloat(trade.p);
-      SetdataBtc(price);
+      socket.onopen = () => {
+        console.log('Conexión WebSocket establecida');
+      };
 
-      await new Promise((resolve) => setTimeout(resolve, 500));
-      SetprevDataBtc(price);
-    };
+      socket.onmessage = async (event) => {
+        const trade = JSON.parse(event.data);
+        const price = parseFloat(trade.p);
+        SetdataBtc(price);
 
-    socket.onclose = () => {
-      console.log('Conexión WebSocket cerrada');
-    };
+        await new Promise((resolve) => setTimeout(resolve, 500));
+        SetprevDataBtc(price);
+      };
 
-    return () => {
-      socket.close();
-    };
+      socket.onclose = () => {
+        console.log('Conexión WebSocket cerrada');
+      };
+
+      resolve(); // Resuelve la promesa una vez que la conexión esté establecida
+
+      return () => {
+        socket.close();
+      };
+    });
   };
 
   useEffect(() => {
@@ -90,15 +104,18 @@ const WebSocketComponent = () => {
 
   return (
     <div>
-      {dataBtc ? <p style={{ color: color }}>Bitcoin: {dataBtc}</p> : false}
-      {showBitcoin && (
-        <button onClick={() => connectWebSocket()}>
-          Ver Precio de Bitcoin
-        </button>
+      {loading ? (
+        <SpinnerLoading />
+      ) : (
+        <>
+          {dataBtc ? <p style={{ color: color }}>Bitcoin: {dataBtc}</p> : null}
+          {dataBtc && (
+            <p>
+              Cambio en 24 h: +{percentagePriceValue} {pricePercentage}%
+            </p>
+          )}
+        </>
       )}
-      <p>
-        Cambio en 24 h: +{percentagePriceValue} {pricePercentage}%
-      </p>
     </div>
   );
 };
